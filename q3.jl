750dcff6cd21d6f393c75e86c648decda5154fbb
Leonardo Sebben <leonardo.sebben.br@gmail.com>
2025-04-01 20:07:51 -0300

    fix q3

--- q3.jl
+++ q3.jl
@@ -13,12 +13,18 @@ m = Model(HiGHS.Optimizer)
 
 @objective(m, Min, 16a + 14b + 22c + 24d + 33x)
 
-@constraint(m, 30a + 35b + 45c + 50d - 62x == 20)
-@constraint(m, 60a + 50b + 35c + 28d - 33x == 30)
-@constraint(m, 10a + 15b + 20c + 22d - 5x == 45)
+@constraint(m, 30a + 35b + 45c + 50d - 62x >= 20)
+@constraint(m, 60a + 50b + 35c + 28d - 33x >= 30)
+@constraint(m, 10a + 15b + 20c + 22d - 5x >= 45)
+@constraint(m, 30a + 35b + 45c + 50d - 62x + 60a + 50b + 35c + 28d - 33x + 10a + 15b + 20c + 22d - 5x == 100)
 
 # Soluciona o modelo
 optimize!(m)
 
 # Mostra os valores encontrados
 @show objective_value(m)
+@show value(a)
+@show value(b)
+@show value(c)
+@show value(d)
+@show value(x)
